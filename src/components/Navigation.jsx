4c415f17b5a5336c60d51625992242856d823503
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2024-07-14 21:26:13 -0700

    :Hide hamburger

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -29,8 +29,10 @@ export const Navigation = () => {
 
       {/* hamburger menu */}
       <button
+        const
+        hiddenClass={menuOpen ? 'hidden' : ''}
         onClick={toggleMenu}
-        className='space-y-1 group md:hidden relative z-50'>
+        className={'space-y-1 group md:hidden relative z-50' + hiddenClass}>
         <div className='w-6 h-1 bg-white'></div>
         <div className='w-6 h-1 bg-white'></div>
         <div className='w-6 h-1 bg-white'></div>
